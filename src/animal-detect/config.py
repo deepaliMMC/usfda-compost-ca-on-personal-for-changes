8a6c77ec550d378d1c34bb6e9b37ef684726b4f0
deepaliMMC <164314374+deepaliMMC@users.noreply.github.com>
2025-10-19 15:55:19 +0530

    Update config.py

--- src/animal-detect/config.py
+++ src/animal-detect/config.py
@@ -5,12 +5,12 @@ from pathlib import Path
 
 class Settings(BaseModel):
     # -------------------- PATHS --------------------
-    DATA_ROOT: Path = Path("/home/sagemaker-user/COMPOST-DETECT-CA/datasets/compost/compost_yoloooo_4band_v1")
-    PROJECT_DIR: Path = Path("/home/sagemaker-user/compost yolo/compost_yolo4_single")
+    DATA_ROOT: Path = Path("/home/sagemaker-user/ANIMAL-DETECT-CA/datasets/animal/animal_yolo_3band_v1")
+    PROJECT_DIR: Path = Path("/home/sagemaker-user/animal-yolo/animal_yolo_single")
     RUN_NAME: str = "yolov8m_4ch"
-    PRETRAINED: Path = Path("/home/sagemaker-user/COMPOST-DETECT-CA/src/compost-detect/data/yolov8m.pt")  # local weights
-    YOLO_MODEL_PATH:Path = Path("/home/sagemaker-user/COMPOST-DETECT-CA/src/compost-detect/data/model.pt")
-    IMAGE_PATH:Path = Path("/home/sagemaker-user/COMPOST-DETECT-CA/test_data/compost/compost_yoloooo_4band_v1")
+    PRETRAINED: Path = Path("/home/sagemaker-user/ANIMAL-DETECT-CA/src/animal-detect/data/yolov8m.pt")  # local weights
+    YOLO_MODEL_PATH:Path = Path("/home/sagemaker-user/ANIMAL-DETECT-CA/src/animal-detect/data/model.pt")
+    IMAGE_PATH:Path = Path("/home/sagemaker-user/ANIMAL-DETECT-CA/test_data/animal/animal_yoloooo_4band_v1")
     # -------------------- TRAINING HYPERPARAMS --------------------
     IMG_SIZE: int = 1024
     EPOCHS: int = 100
@@ -22,15 +22,15 @@ class Settings(BaseModel):
     BUCKET: str = "objectdetction-ca"
     #-------------------- DATA GENERATION --------------------
     NAIP_PREFIX: str = "NAIP_Imagery_2024/"
-    POS_GEOJSON: str  = "compost_polygons.geojson"
+    POS_GEOJSON: str  = "positive_polygons.geojson"
     NEG_GEOJSON: str  = "neg_points.geojson"
-    OUT_PREFIX : str  = "compost_yolo_4band_v1"
-    CHIP_SIZE: int    = 1024
+    OUT_PREFIX : str  = "animal_yolo_band_v1"
+    CHIP_SIZE: int    = 512
     NEG_BUFFER_M: int = 200.0
     POS_CLASS_ID: int = 0
     MAX_WORKERS: int  = int(os.environ.get("MAX_WORKERS", "24"))
     # -------------------- CLASS --------------------
-    CLASS_NAMES: list[str] = ["compost"]
+    CLASS_NAMES: list[str] = ["animal"]
 
     # -------------------- Validators / Init --------------------
     @field_validator("PROJECT_DIR", "DATA_ROOT", mode="before")
